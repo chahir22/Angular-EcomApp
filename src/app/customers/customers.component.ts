044338806e79b34cd5b279174bbad92a214369a0
chahir22 <simocha166@gmail.com>
2024-12-09 21:10:16 +0100

    first commit

---
+++ src/app/customers/customers.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+@Component({
+  selector: 'app-customers',
+  templateUrl: './customers.component.html',
+  styleUrl: './customers.component.css'
+})
+export class CustomersComponent implements OnInit {
+
+  customers : any;
+  constructor(private http: HttpClient) { }
+  ngOnInit(): void {
+    this.http.get('http://localhost:9999/CUSTOMER-SERVICE/customers').subscribe({
+      next: data => {
+        this.customers = data;
+      },
+      error: err => {}
+    })
+  }
+
+
+  getOrders() {
+
+  }
+}
